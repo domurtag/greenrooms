9316240d3130eae236adbb17ee4f5bb0c9e10367
Donal Murtagh <domurtag@yahoo.co.uk>
2013-09-17 00:27:33 +0100

    removed server-side param validation

--- contact/mail.php
+++ contact/mail.php
@@ -5,15 +5,11 @@ require 'class.phpmailer.php';
 $myemail = "domurtag@yahoo.co.uk";
 
 /* Check all form inputs using check_input function */
-$name    = check_input($_POST['name'], "Invalid 'name' field");
-$email   = check_input($_POST['email']);
+$name    = $_POST['name'];
+$email   = $_POST['email'];
 $subject = "Green Rooms Design feedback from: $name, $email";
-$message = check_input($_POST['message'], "Invalid 'message' field");
+$message = $_POST['message'];
 
-/* If e-mail is not valid show error message */
-if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email)) {
-    show_error("E-mail address not valid");
-}
 /* Let's prepare the message for the e-mail */
 $message = "Sender Name: $name
 Sender E-mail: $email
@@ -40,7 +36,7 @@ function smtpmailer($to, $from, $from_name, $subject, $body) {
     $mail->Username = 'festivals@festivals.ie';
 
     // TODO don't commit password
-    $mail->Password = 'blackpear1';
+    $mail->Password = '';
     $mail->SetFrom($from, $from_name);
     $mail->Subject = $subject;
     $mail->Body = $body;
@@ -48,17 +44,6 @@ function smtpmailer($to, $from, $from_name, $subject, $body) {
     if (!$mail->Send()) {
         show_error($mail->ErrorInfo);
     }
-}
-
-function check_input($data, $problem = '')
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    if ($problem && strlen($data) == 0) {
-        show_error($problem);
-    }
-    return $data;
 }
 
 function show_error($myError)
