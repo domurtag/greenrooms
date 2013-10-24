ef4fa94ce613fa667b30aafe9d9f6e9c42231e38
Donal Murtagh <domurtag@yahoo.co.uk>
2013-10-24 21:34:09 +0100

    slider starts on a random image and transitions more quickly

--- wp-content/themes/OrchardKeepers/js/myscript.js
+++ wp-content/themes/OrchardKeepers/js/myscript.js
@@ -1,31 +1,24 @@
-jQuery(document).ready(function($){
-	$('.home_slider').bxSlider({
-	  auto: true,
-	  pause: 6000,
-	  autoControls: true
-	});
-	
-	$('.location_hire_slider').bxSlider({
-	  auto: true,
-	  pause: 6000,
-	  autoControls: true
-	});
-	
-	$(".gallery-icon a").colorbox({rel:'group1', width:"30%", height:"auto"});
-	
-	$(".make_a_booking").click(function(){
-	
-		if(jQuery(this).parent().find(".booking_form").css("display")!="block")
-		{
-			jQuery(this).parent().find(".booking_form").slideDown("fast");
-		}
-		else
-		{
-			jQuery(this).parent().find(".booking_form").slideUp("fast");
-		}
-		$(this).slideUp("fast");
-		return false;
-		
-	});
-	
+jQuery(document).ready(function ($) {
+    $('.home_slider').bxSlider({
+        auto: true,
+        pause: 3000,
+        autoControls: true,
+        randomStart: true
+    });
+
+    $(".gallery-icon a").colorbox({rel: 'group1', width: "30%", height: "auto"});
+
+    $(".make_a_booking").click(function () {
+
+        if (jQuery(this).parent().find(".booking_form").css("display") != "block") {
+            jQuery(this).parent().find(".booking_form").slideDown("fast");
+        }
+        else {
+            jQuery(this).parent().find(".booking_form").slideUp("fast");
+        }
+        $(this).slideUp("fast");
+        return false;
+
+    });
+
 });
