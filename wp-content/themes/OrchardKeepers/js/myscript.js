78cc78fd20e9b639b92fc717da1ee6dc540a005f
Donal Murtagh <domurtag@yahoo.co.uk>
2013-10-24 21:41:02 +0100

    removed unused CSS/JS

--- wp-content/themes/OrchardKeepers/js/myscript.js
+++ wp-content/themes/OrchardKeepers/js/myscript.js
@@ -5,20 +5,4 @@ jQuery(document).ready(function ($) {
         autoControls: true,
         randomStart: true
     });
-
-    $(".gallery-icon a").colorbox({rel: 'group1', width: "30%", height: "auto"});
-
-    $(".make_a_booking").click(function () {
-
-        if (jQuery(this).parent().find(".booking_form").css("display") != "block") {
-            jQuery(this).parent().find(".booking_form").slideDown("fast");
-        }
-        else {
-            jQuery(this).parent().find(".booking_form").slideUp("fast");
-        }
-        $(this).slideUp("fast");
-        return false;
-
-    });
-
 });
